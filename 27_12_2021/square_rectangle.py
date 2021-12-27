5851244fa3d007c673f1d5322cf4b452d252722c
sai2yeshwanth <yashsai222@gmail.com>
2021-12-27 17:38:26 +0530

    Create square_rectangle.py

---
+++ 27_12_2021/square_rectangle.py
@@ -0,0 +1,13 @@
+# Given the length and breadth of a box.
+#check if it is a Rectangle or Square
+
+print("Enter the length of the box :")
+length = int(input())
+
+print("Enter the breadth of the box :")
+breadth = int(input())
+
+if length == breadth :
+    print("Square")
+else:
+    print("Rectangle")
