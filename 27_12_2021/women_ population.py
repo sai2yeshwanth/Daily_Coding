049d6c83f2c4e83f91cef9e0af5dfc27f0b0a180
sai2yeshwanth <yashsai222@gmail.com>
2021-12-27 09:52:30 +0530

    Create women_ population.py

---
+++ 27_12_2021/women_ population.py
@@ -0,0 +1,8 @@
+#In a town, the percentage of men is 52 and the rest are women. the total population o town is given as input.
+# Write a program to print the total number of women in the town
+print("Total percentage in Town")
+total_population = int(input())
+
+women_population = (total_population*48)/100
+
+print("Percentage of Women in town :" , int(women_population))
