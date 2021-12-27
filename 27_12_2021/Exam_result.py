b4b8b875a184cb49ea30af2afb844a3d8c1a6f47
sai2yeshwanth <yashsai222@gmail.com>
2021-12-27 11:26:11 +0530

    Create Exam_result.py

---
+++ 27_12_2021/Exam_result.py
@@ -0,0 +1,9 @@
+# write a program that read the student's marks as input amd prints
+# pass or fail if the student has scored more than 50, print PASS.in all other cases print FAIL.
+
+print("Enter the Marks :")
+marks = int(input())
+if marks > 50 :
+    print("Pass")
+else:
+    print("Fail") 
