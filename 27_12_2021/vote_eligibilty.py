743ccba059e80e2c7e72ba05c3504ed58a22479b
sai2yeshwanth <yashsai222@gmail.com>
2021-12-27 17:24:59 +0530

    Create vote_eligibilty.py

---
+++ 27_12_2021/vote_eligibilty.py
@@ -0,0 +1,10 @@
+# A person is eligibily to vote her/his is at least 18 years.
+# write a program the check if a person is eligible to vote base on the person's age
+
+print("Enter the Age of the Person : ")
+age_person = int(input())
+
+if age_person >= 18 :
+    print("Eligible to Vote")
+else:
+    print("Not Eligible to Vote")
