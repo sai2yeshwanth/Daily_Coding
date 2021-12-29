89c98bb49950927ad451a57d3aad3a56b66cb933
sai2yeshwanth <yashsai222@gmail.com>
2021-12-29 10:17:14 +0530

    Create positive_or_negative.py

---
+++ 29_12_2021/positive_or_negative.py
@@ -0,0 +1,8 @@
+# A number N is given as input. Write a program to check if N is positive or Negative
+# the input will be in float
+num = float(input())
+condition = num>0
+if condition :
+    print("Positive")
+else:
+    print("Negative")
