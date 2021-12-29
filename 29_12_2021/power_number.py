7f98742c0999e01f5ea65e5e915d12f41d48d424
sai2yeshwanth <yashsai222@gmail.com>
2021-12-29 20:10:37 +0530

    Create power_number.py

---
+++ 29_12_2021/power_number.py
@@ -0,0 +1,9 @@
+# Given two positive integer , Which is anumber and a power. Write a program to calculate the power of the given number.
+
+print("Enter the Numbers :")
+A = int(input())
+B  = int(input())
+
+condition = A**B
+
+print("The Power of the given two numbers :",condition)
