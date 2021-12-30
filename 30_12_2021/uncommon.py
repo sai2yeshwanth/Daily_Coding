6c178582988755013c6ed61eccf1d92aaaa8c92e
sai2yeshwanth <yashsai222@gmail.com>
2021-12-30 10:43:05 +0530

    Update uncommon.py

--- 30_12_2021/uncommon.py
+++ 30_12_2021/uncommon.py
@@ -1,8 +1,8 @@
 # Given a number N, find whether the number is common or uncommon. A number is considered uncommon if 
 # it is not divisible by any single-digit prime
 
-print("Enter the Number :")
-number = int(input())
+
+number = int(input("Enter the number : "))
 
 condition = (number%2==0 or number%3==0 or number%5==0 or number%7==0 )
 
