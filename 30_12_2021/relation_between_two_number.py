45b071f4b713adc4683288d623496826aea36cb4
sai2yeshwanth <yashsai222@gmail.com>
2021-12-30 11:24:46 +0530

    Create relation_between_two_number.py

---
+++ 30_12_2021/relation_between_two_number.py
@@ -0,0 +1,11 @@
+# Given two integer A and B, write a program to print the relation Between the two number:
+
+a = int(input("Enter the Number A : "))
+b = int(input("Enter the Number B : "))
+if a == b:
+    print("A == B")
+elif a < b:
+    print("A < B")
+elif a > b:
+    print("A > B")
+    
