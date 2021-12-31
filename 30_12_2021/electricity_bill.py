e5c6894e51314c07a6ae64c57290d2f056eca678
sai2yeshwanth <yashsai222@gmail.com>
2021-12-31 07:48:05 +0530

    Update electricity_bill.py

--- 30_12_2021/electricity_bill.py
+++ 30_12_2021/electricity_bill.py
@@ -14,5 +14,7 @@ elif units >= 250:
     bill = (50 * 2) +(3*100)+(5*100)+(8*(units-250))
     
 subcharge =(0.2*bill)
+print("Units Charge bill is :",bill)
+print("Subcharge :",subcharge)
 total_bill = (bill + subcharge)
-print(total_bill)
+print("total Electricity :",total_bill)
