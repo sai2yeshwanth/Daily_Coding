5ed8a640ea03b169ca88749dc2765531cc05faf5
sai2yeshwanth <yashsai222@gmail.com>
2022-01-01 20:43:45 +0530

    Create while_loop_practice_2.py

---
+++ 1-1-2022/while_loop_practice_2.py
@@ -0,0 +1,9 @@
+#Write a program to print interger from m number to n number 
+
+m = int(input("Enter the staring number : "))
+n = int(input("Enter the ending number : "))
+
+while m<=n:
+    print(m)
+    m+=1
+    
