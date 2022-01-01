0e596b8281c2a3e3bf256135e36b2f6b4805ae8d
sai2yeshwanth <yashsai222@gmail.com>
2022-01-01 20:43:42 +0530

    Create solid_square_pattern_using_for_loop.py

---
+++ 1-1-2022/solid_square_pattern_using_for_loop.py
@@ -0,0 +1,6 @@
+# print the solid square pattern of N number of rows and N number of column using for loop
+
+N = int(input("Enter the number to print Solid square pattern : "))
+
+for i in range(1,N+1):
+    print("* " *N)
