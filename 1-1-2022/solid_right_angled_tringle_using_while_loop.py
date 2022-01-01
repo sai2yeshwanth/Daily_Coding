6798dd6a6c86c2fb7b8dae4376ed2c468173c937
sai2yeshwanth <yashsai222@gmail.com>
2022-01-01 23:04:08 +0530

    Create solid_right_angled_tringle_using_while_loop.py

---
+++ 1-1-2022/solid_right_angled_tringle_using_while_loop.py
@@ -0,0 +1,7 @@
+integer = int(input("Please enter the number of N line : "))
+
+counter = 1
+
+while counter <= integer:
+    print("* " * counter) #we are multiplying asterisk with counter to make a right-angled triangular pattern
+    counter = counter+1
