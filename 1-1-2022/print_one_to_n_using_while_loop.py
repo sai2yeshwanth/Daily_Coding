63ff50549bd626a7011fe15e2e8293d1184be648
sai2yeshwanth <yashsai222@gmail.com>
2022-01-01 20:43:30 +0530

    Create print_one_to_n_using_while_loop.py

---
+++ 1-1-2022/print_one_to_n_using_while_loop.py
@@ -0,0 +1,10 @@
+# Write a program to print integers from i to the given number(N).
+# using while-loop
+
+integer = int(input("Enter the number"))
+
+counter = 1
+
+while counter <= integer:
+    print(counter)
+    counter += 1
