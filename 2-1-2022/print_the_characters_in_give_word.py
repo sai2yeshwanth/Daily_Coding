fd64c2915b0a81b6bef300b45d3b19331f914896
sai2yeshwanth <yashsai222@gmail.com>
2022-01-02 09:52:45 +0530

    Create print_the_characters_in_give_word.py

---
+++ 2-1-2022/print_the_characters_in_give_word.py
@@ -0,0 +1,7 @@
+# Write a program that read a word and print each character of the word in a new line
+#using for-loop
+
+string = input("Please enter the  word : ")
+
+for i in string:
+    print(i)
