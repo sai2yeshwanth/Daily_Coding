43151cf544959240c89d4fc75aa84d63374f33db
sai2yeshwanth <yashsai222@gmail.com>
2022-01-02 09:53:21 +0530

    Create read_N_input_using_for_loop.py

---
+++ 2-1-2022/read_N_input_using_for_loop.py
@@ -0,0 +1,8 @@
+# given an integer N, write a program which read N inputs and prints them
+
+N = int(input("Enter the number of input to read : "))
+
+for i in range(N):
+    inputs = input("Enter the input to read : ")
+    print(inputs)
+    
