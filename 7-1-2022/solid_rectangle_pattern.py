f5ad3f84cc5ca8bb5e873e001be4178f776e6482
sai2yeshwanth <yashsai222@gmail.com>
2022-01-07 12:13:39 +0530

    Update solid_rectangle_pattern.py

--- 7-1-2022/solid_rectangle_pattern.py
+++ 7-1-2022/solid_rectangle_pattern.py
@@ -2,8 +2,15 @@
 # Write a program to print a rectangle pattern of M rows and N columns using the plus character(+).
 # note: Ther is space after each plus (+) character.
 
+def Solid_tringle_pattern_plus(M,N):
+    
+    for num in range(M):
+        print("+ "*N)
+
+    return
+    
+
 M = int(input("Enter the Number of Rows : "))            # number of Rows
 N = int(input("Enter the Number of Columns : "))            # Number of columns
 
-for num in range(1,M+1):        
-    print("+ "* N)
+Solid_tringle_pattern_plus(M, N)
