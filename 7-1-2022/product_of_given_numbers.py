4781546b74f9d5f9a7e1f816f79ddfa82e40b2ac
sai2yeshwanth <yashsai222@gmail.com>
2022-01-07 12:03:10 +0530

    Update product_of_given_numbers.py

--- 7-1-2022/product_of_given_numbers.py
+++ 7-1-2022/product_of_given_numbers.py
@@ -1,12 +1,17 @@
 # Product of the Given Numbers
 # Given an integer N, Write a program which reads N inputs and prints the product of the given input integers.
 
+def product_of_the_given_number(N):
+    product = 1
+
+    for num in range(1,N+1):
+        number = int(input("Enter the {} Number : ".format(num)))  #input
+        product *=  number      #muliplying the each input with product
+
+    print("The product of the given number's :",product)
+    return
+
+
+
 N = int(input("Enter the number to Read the number of inputs : "))        # to read the number  of inputs
-
-product = 1
-
-for num in range(1,N+1):
-    number = int(input("Enter the {} Number : ".format(num)))  #input
-    product *=  number      #muliplying the each input with product
-
-print("The product of the given number's :",product)
+product_of_the_given_number(N)
