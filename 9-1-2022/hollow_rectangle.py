89e2b5fae292b6ccd02e9bffd46421615db1eaa5
sai2yeshwanth <yashsai222@gmail.com>
2022-01-09 08:59:01 +0530

    Create hollow_rectangle.py

---
+++ 9-1-2022/hollow_rectangle.py
@@ -0,0 +1,20 @@
+# Hollow Rectangle
+# Given two integers M and N, Write a program to print a hollow number rectangle pattern of
+# M rows and N columns using the asterisk character(*)
+
+def hollow_rectangle(rows,columns):
+
+    print("{0} X {1} Hollow Rectangle is below")
+
+    print("* "*columns)
+    
+    for i in range(rows-2):
+        print("* "+("  "*(columns-2))+"* ")
+    
+    print("* "* columns)
+    return
+
+M = int(input("Enter the Number of Rows : "))
+N = int(input("Enter the Number of Columns : "))
+
+hollow_rectangle(M,N)
