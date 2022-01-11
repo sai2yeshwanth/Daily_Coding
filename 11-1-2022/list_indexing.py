bd47237727c30013c844bfb49ebb31aea484b49f
sai2yeshwanth <yashsai222@gmail.com>
2022-01-11 23:50:54 +0530

    Update list_indexing.py

--- 11-1-2022/list_indexing.py
+++ 11-1-2022/list_indexing.py
@@ -4,8 +4,9 @@
 def list_indexing(index_number):
     color_list = ["Red", "Orange", "Yellow", "Pink", "Green", "Blue", "Purple", "Black", "White"]
     # Write your code here
+    print("Color List is {}".format(color_list))
     if index_number >=0 and index_number <=8:
-        print(color_list[index_number])
+        print("you enter index number is {0} and color is {1}".format(index_number,color_list[index_number]))
     else:
         print("please enter the number betweem 0 to 8 ")
     return
