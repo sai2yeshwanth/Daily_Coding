faa750d11e798b2cfd631cd99033f16faac4ce89
sai2yeshwanth <yashsai222@gmail.com>
2022-01-12 00:45:58 +0530

    Delete covernt_string_list.py

--- 11-1-2022/covernt_string_list.py
+++
@@ -1,9 +0,0 @@
-# Convert string to list
-
-def convert_string_to_list(word):
-    list_a = list(word)
-    print("The word {} is covernt to list {}".format(word,list_a))
-    
-    return
-word = input("Enter the word to convert into list : ")
-convert_string_to_list(word)
