8c1227848483d0ab16cf102fa9339f40e197bc7c
sai2yeshwanth <yashsai222@gmail.com>
2022-01-11 22:03:12 +0530

    Create create_and print_list.py

---
+++ 11-1-2022/create_and print_list.py
@@ -0,0 +1,8 @@
+# Create and print list
+# Write a program to print a list with the given elements "Rose", 183, 148, 123.64, False
+
+def create_and_print_list(list_a):
+    print(list_a)
+    return
+list_a = ["Rose", 183, 148, 123.64, False]
+create_and_print_list(list_a)
