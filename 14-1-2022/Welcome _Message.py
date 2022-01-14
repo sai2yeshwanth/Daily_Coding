64a723d3738942f9038033cccec77fe3903ef97a
sai2yeshwanth <yashsai222@gmail.com>
2022-01-14 23:38:31 +0530

    Create Welcome _Message.py

---
+++ 14-1-2022/Welcome _Message.py
@@ -0,0 +1,8 @@
+# Welcome Message
+
+def say_wishes(arg_1):
+    # print the Welcome messuge using arguments
+    print("Welcome"+ " " +arg_1 )
+
+name = input("Enter the name : ")
+say_wishes(name)
