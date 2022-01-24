c1dc7074425fb3de8305ffee10bb14f929de421a
sai2yeshwanth <yashsai222@gmail.com>
2022-01-24 17:14:12 +0530

    Create nested_list_indexing.py

---
+++ 24-1-2021/nested_list_indexing.py
@@ -0,0 +1,10 @@
+# Nested list Indexing
+
+num_list = [(2, 4, 6, 8), (5, 15, 25, 35), (7, 14, 21)]
+# Write your code here
+n = int(input("Enter the Number in num_list :"))
+for tuple_a in num_list:
+    if n in tuple_a:
+        tuple_index = num_list.index(tuple_a)
+        n_index = tuple_a.index(n)
+        print(f"{n} in {tuple_index} {n_index}")
